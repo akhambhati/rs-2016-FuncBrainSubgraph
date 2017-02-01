267805fa708eda1d7bcc846a27fb09e8edb5b3cd
Ankit N. Khambhati <akhambhati@gmail.com>
2017-02-01 10:15:12 -0500

    Updated

--- Analysis_Notebooks/e02b_Module_Detection.m
+++ Analysis_Notebooks/e02b_Module_Detection.m
@@ -2,65 +2,38 @@
 clear; clc;
 
 load ~/Remotes/RSRCH.NMF_Subnetworks/e02b-DynFuncModule-Population/Module_Optimization.AdjMatr.mat
+adj_matr = adj_matr(1:80*5,:,:);
 
 % Convert adj_matr to cell array
 for s=1:length(adj_matr)
     A{s} = squeeze(adj_matr(s, :, :));
 end
-clear s adj_matr;
 
 % Convert adj_name to cell array
-for s=1:length(adj_name)
+for s=1:length(adj_matr)
     Label{s} = strsplit(adj_name(s, :), '.');
 end
-clear s adj_name;
 
-%% Generate modularity matrix (categorical for func handle)
-gamma = 1.0;
-omega = 1.0;
-
-N=length(A{1});
-T=length(A);
-ii=[]; jj=[]; vv=[];
-twomu=0;
-for s=1:T
-    indx=[1:N]'+(s-1)*N;
-    [i,j,v]=find(A{s});
-    ii=[ii;indx(i)]; jj=[jj;indx(j)]; vv=[vv;v];
-    k=sum(A{s});
-    kv=zeros(N*T,1);
-    twom=sum(k);
-    twomu=twomu+twom;
-    kv(indx)=k/twom;
-    kcell{s}=kv;
+% Find new_subject indices
+new_subject(1) = 0;
+for s=2:length(adj_matr)
+    if Label{s-1}{1} == Label{s}{1}
+        new_subject(s) = 0;
+    else
+        new_subject(s) = 1;
+    end
 end
-AA = sparse(ii,jj,vv,N*T,N*T);
-clear ii jj vv
-kvec = full(sum(AA));
-all2all = N*[(-T+1):-1,1:(T-1)];
-AA = AA + omega*spdiags(ones(N*T,2*T-2),all2all,N*T,N*T);
-twomu=twomu+T*omega*N*(T-1);
-save('~/Remotes/RSRCH.NMF_Subnetworks/e02b-DynFuncModule-Population/Module_Optimization.ModMat_FnHandle.mat', ...
-     'AA', 'kcell', 'kvec', 'N', 'T', 'gamma', 'omega', 'twomu', '-v7.3')
-
-%% Perform genlouvain (categorical for func handle)
-clear; clc; 
-
-load ~/Remotes/RSRCH.NMF_Subnetworks/e02b-DynFuncModule-Population/Module_Optimization.ModMat_FnHandle.mat
-
-B = @(i) AA(:,i) - gamma*kcell{ceil(i/(N+eps))}*kvec(i);
-[S,Q] = genlouvain(B);
-Q = Q/twomu
-S = reshape(S,N,T);
-imagesc(S);
+time_subj = find(new_subject)-1;
+clear s adj_matr adj_name;
 
 %% Generate modularity matrix (categorical for full matrix)
 gamma = 1.0;
-omega = 5.0;
+omega = 1.0;
+rho = 0.00001;
 
 N=length(A{1});
 T=length(A);
-B=spalloc(N*T,N*T,(N+T)*N*T);
+B=spalloc(N*T,N*T,N*N*T+2*N);
 twomu=0;
 for s=1:T
     k=sum(A{s});
@@ -69,18 +42,44 @@ for s=1:T
     indx=[1:N]+(s-1)*N;
     B(indx,indx)=A{s}-gamma*k'*k/twom;
 end
-twomu=twomu+T*omega*N*(T-1);
+twomu=twomu+2*omega*N*(T-1);
+
+% Link time points
+Tsubj = [1, N*time_subj, N*T];
+B = B + omega*spdiags(ones(N*T,2),[-N,N],N*T,N*T);
+for ii=2:length(Tsubj)-1
+    B(Tsubj(ii)+1:Tsubj(ii)+1+N, Tsubj(ii)+1-N:Tsubj(ii)+1) = 0;
+    B(Tsubj(ii)+1-N:Tsubj(ii)+1, Tsubj(ii)+1:Tsubj(ii)+1+N) = 0;
+end
+
+% Link subjects
 all2all = N*[(-T+1):-1,1:(T-1)];
-B = B + omega*spdiags(ones(N*T,2*T-2),all2all,N*T,N*T);
+B_all2all = rho*spdiags(ones(N*T,2*T-2),all2all,N*T,N*T);
+for ii=1:length(Tsubj)-1
+    B_all2all(Tsubj(ii):Tsubj(ii+1), Tsubj(ii):Tsubj(ii+1)) = 0;    
+end
+
+% Add back subjects
+B = B + B_all2all;
+
 save('~/Remotes/RSRCH.NMF_Subnetworks/e02b-DynFuncModule-Population/Module_Optimization.ModMat_Full.mat', ...
-     'B', 'N', 'T', 'gamma', 'omega', 'twomu', '-v7.3')
+     'B', 'N', 'T', 'time_subj', 'gamma', 'omega', 'twomu', '-v7.3')
  
  %% Perform genlouvain (categorical for full matrix)
-clear; clc; 
+clear; clc;
 
 load ~/Remotes/RSRCH.NMF_Subnetworks/e02b-DynFuncModule-Population/Module_Optimization.ModMat_Full.mat
 
 [S,Q] = genlouvain(B);
 Q = Q/twomu
-S = reshape(S,N,T);
-imagesc(S);
+S = reshape(S, N, T);
+
+time_subj = [0, time_subj, T];
+for ii=1:length(time_subj)-1
+    Ssubj{ii} = S(:, time_subj(ii)+1:time_subj(ii+1));
+end    
+
+for ii=1:length(Ssubj)
+    figure();
+    imagesc(Ssubj{ii});
+end
